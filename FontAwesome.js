1b387c4eb5ab110713a40885350198420f8c4401
Dz8Web <40293615+Dz8Web@users.noreply.github.com>
2018-07-20 16:08:11 +0100

    Create FontAwesome.js

---
+++ FontAwesome.js
@@ -0,0 +1,10 @@
+//<![CDATA[
+$(document)['ready'](function () {
+        $('#Ahmed47')['html']('<a href="https://www.facebook.com/ahmedabderahman47" rel="dofollow" target="_blank">A-Abderahman</a>');
+        setInterval(function () {
+                if (!$('#Ahmed47:visible')['length']) {
+                  window['location']['href'] = 'https://www.facebook.com/ahmedabderahman47';
+                };
+            }, 1000);
+    });
+//]]>
